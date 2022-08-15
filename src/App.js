91f1f1fdadc5ab991386c46e7d440da92a584c39
shahin <shahin.shahi1986@gmail.com>
2022-08-15 16:42:35 +0300

    make and fixed bag of router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import {Navbar} from './components'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Cryptoverse</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   )
 }
