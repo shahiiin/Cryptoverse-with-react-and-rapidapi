c7c5293aa51387a11fe7b91bf0d66e8563cda540
shahin <shahin.shahi1986@gmail.com>
2022-08-15 20:13:10 +0300

    make provider with redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import App from './App';
-
-
-import 'antd/dist/antd.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import 'antd/dist/antd.css'
+import { Provider } from 'react-redux'
+import store from './app/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      
+      <Provider store={store}>
         <App />
-    
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
-);
+)
---
+++ src/services/criptoApi.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       limit: '50',
+//       offset: '0',
+//       orderBy: '24hVolume',
+//       orderDirection: 'desc'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': '079e150c67mshe04008bbadb9cb8p1429bajsnb83adbde9277',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
